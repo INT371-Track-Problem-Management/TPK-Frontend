71dd132271ec0753fa17e5f8f8e2977d98cd5e70
Sun <59905230+ArtidSun@users.noreply.github.com>
2022-05-14 14:46:23 +0700

    edit HomePage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '../views/Homepage.vue'
+import HomePage from '../views/HomePage.vue'
 import LoginPage from '../views/LoginPage.vue'
 import RegisterPage from '../views/register/RegisterPage.vue'
 import RegisterSelectPage from '../views/register/RegisterSelectPage.vue'
